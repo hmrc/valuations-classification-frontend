ad9f69e9d8903e6b8e08cb7b17dd07725cdc7af2
Robert Udah <airudah2000@gmail.com>
2023-02-13 14:56:34 +0000

    Revert "Initial commit"
    This reverts commit 80599efd3640e72b063d7063b427255eb1f22897.

--- app/assets/javascripts/search.js
+++
@@ -1,8 +0,0 @@
-var advancedSearch = {
-    removeKeyword: function(index) {
-        var tbody = document.getElementById("advanced_search_keywords-list");
-        var row = document.getElementById("advanced_search_keywords-list-row-" + index);
-        tbody.removeChild(row);
-        document.getElementById("advanced_search-form").submit();
-    }
-};
--- run_all_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sbt clean compile scalafmtAll scalastyleAll coverage Test/test IntegrationTest/test dependencyUpdates coverageReport
-
