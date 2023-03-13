fe3f030b86c8b2cf696fda7f9a8c9dd55d3dae56
doug.maxwell <douglasj.maxwell@icloud.com>
2023-03-13 09:38:03 +0000

    updated controller

---
+++ app/avar2/controllers/AllNewCasesController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package avar2.controllers
+
+import avar2.controllers.actions.AuthenticatedCaseWorkerAction
+import avar2.models.viewmodels.CasesTabViewModel
+import avar2.services.ValuationCaseService
+import avar2.views.html.open_cases_view
+import com.google.inject.Inject
+import config.AppConfig
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+
+import scala.concurrent.ExecutionContext
+
+
+class AllNewCasesController @Inject()(
+                                         verify: AuthenticatedCaseWorkerAction,
+                                         valuationCaseService: ValuationCaseService,
+                                         mcc: MessagesControllerComponents,
+                                         val openCasesView: open_cases_view,
+                                         implicit val appConfig: AppConfig,
+                                         implicit val ec: ExecutionContext
+) extends FrontendController(mcc)
+    with I18nSupport {
+
+  def displayAllNewCases(activeSubNav: avar2.models.viewmodels.AvarSubNavigationTab = avar2.models.viewmodels.AVaRTab): Action[AnyContent] =
+   // (verify.authenticated andThen verify.mustHave(Permission.VIEW_QUEUE_CASES))
+      verify.async { implicit request =>
+      for {
+        cases <- valuationCaseService.allNewValuationCases()
+
+        openCases = CasesTabViewModel.create(cases)
+
+      } yield Ok(openCasesView(openCases, activeSubNav))
+    }
+}
