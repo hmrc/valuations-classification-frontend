129147f1f826dd0a820008fd130a0773130086c9
Robert Udah <airudah2000@gmail.com>
2023-03-13 15:50:35 +0000

    Implemented changing status to Rejected

--- app/avar2/controllers/ChangeCaseStatusController.scala
+++ app/avar2/controllers/ChangeCaseStatusController.scala
@@ -18,7 +18,8 @@ package avar2.controllers
 
 import avar2.controllers.actions.AuthenticatedCaseWorkerAction
 import avar2.forms.CaseStatusRadioInputFormProvider
-import avar2.services.CasesService
+import avar2.models.{CaseStatusRadioInput, ValuationCase}
+import avar2.services.ValuationCaseService
 import avar2.views.html.change_case_status
 import config.AppConfig
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
@@ -26,47 +27,58 @@ import uk.gov.hmrc.play.bootstrap.controller.WithUnsafeDefaultFormBinding
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.Future.successful
 
 class ChangeCaseStatusController @Inject()(
                                             verify: AuthenticatedCaseWorkerAction,
-                                            casesService: CasesService,
+                                            valuationCaseService: ValuationCaseService,
                                             mcc: MessagesControllerComponents,
                                             val change_case_status: change_case_status,
                                             implicit val appConfig: AppConfig
 )(implicit val executionContext: ExecutionContext)
     extends FrontendController(mcc)
-   // with RenderCaseAction
+    with RenderCaseAction
     with WithUnsafeDefaultFormBinding {
 
- // override protected val config: AppConfig         = appConfig
- // override protected val caseService: CasesService = casesService
+  override protected val config: AppConfig         = appConfig
+  override protected val caseService: ValuationCaseService = valuationCaseService
 
   val form = new CaseStatusRadioInputFormProvider()()
 
-  def onPageLoad(reference: String): Action[AnyContent] = ???
+  def onPageLoad(reference: String): Action[AnyContent] =
 //    (verify.authenticated andThen
 //      verify.casePermissions(reference) andThen
-//      verify.mustHave(Permission.EDIT_RULING)).async { implicit request =>
-//      validateAndRenderView(c => successful(change_case_status(c, form)))
-//    }
+//      verify.mustHave(Permission.EDIT_RULING)).async
+  verify.async{ implicit request =>
+    valuationCaseService.valuationCase(reference).flatMap {
+      case Some(vc: ValuationCase) => validateAndRenderView(vc => successful(change_case_status(vc, form)))(request, vc)
+      case None => throw new Exception("failed to load case view")
+    }
+  }
 
-  def onSubmit(reference: String): Action[AnyContent] = ???
+  def onSubmit(reference: String): Action[AnyContent] =
 //    (verify.authenticated andThen
 //      verify.casePermissions(reference) andThen
-//      verify.mustHave(Permission.EDIT_RULING)) { implicit request =>
-//      form
-//        .bindFromRequest()
-//        .fold(
-//          hasErrors => Ok(change_case_status(request.`case`, hasErrors)), {
-//            case CaseStatusRadioInput.Complete => Redirect(routes.CompleteCaseController.completeCase(reference))
-//            case CaseStatusRadioInput.Refer    => Redirect(routes.ReferCaseController.getReferCaseReason(reference))
-//            case CaseStatusRadioInput.Reject   => Redirect(routes.RejectCaseController.getRejectCaseReason(reference))
-//            case CaseStatusRadioInput.Suspend  => Redirect(routes.SuspendCaseController.getSuspendCaseReason(reference))
-//            case CaseStatusRadioInput.MoveBackToQueue =>
-//              Redirect(routes.ReassignCaseController.reassignCase(reference, request.uri))
-//          }
-//        )
-//    }
+//      verify.mustHave(Permission.EDIT_RULING)).async
+    verify.async { implicit request =>
+
+      valuationCaseService.valuationCase(reference).flatMap {
+        case Some(vc: ValuationCase) => {
+          form
+            .bindFromRequest()
+            .fold(
+              hasErrors => Future.successful(Ok(change_case_status(vc, hasErrors))),
+              {
+                //            case CaseStatusRadioInput.Complete => Redirect(routes.CompleteCaseController.completeCase(reference))
+                //            case CaseStatusRadioInput.Refer    => Redirect(routes.ReferCaseController.getReferCaseReason(reference))
+                case CaseStatusRadioInput.Reject   => Future.successful(Redirect(routes.RejectCaseController.getRejectCaseReason(reference)))
+                //            case CaseStatusRadioInput.Suspend  => Redirect(routes.SuspendCaseController.getSuspendCaseReason(reference))
+//                case CaseStatusRadioInput.MoveBackToQueue => Redirect(routes.ReassignCaseController.reassignCase(reference, request.uri))
+              }
+            )
+        }
+        case None => throw new Exception("failed to load case view")
+      }
+    }
 }
